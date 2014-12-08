8f6d8e203ea78e902ceb50da7f6bfeb3a06ccc1e
William Duyck <fuzzyfox0@gmail.com>
2014-12-08 13:51:08 +0000

    Added fullscreen mode + made unicode work.
    Just because I can really.

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,4 +16,41 @@
     }
     return prev(callback);
   }
+
+  window.addEventListener( 'keyup', function( event ) {
+    if( event.key === 'F' ) {
+      document.documentElement.requestFullscreen();
+    }
+  });
 }() );
+
+
+(function(){
+  var fullscreenEnabled = document.fullscreenEnabled ||
+                          document.webkitFullscreenEnabled ||
+                          document.mozFullScreenEnabled ||
+                          document.msFullscreenEnabled;
+
+  var requestFullscreen = function( element ) {
+    if( element.requestFullscreen ) {
+      element.requestFullscreen();
+    }
+    else if( element.webkitReqestFullscreen ) {
+      element.webkitReqestFullscreen();
+    }
+    else if( element.mozRequestFullScreen ) {
+      element.mozRequestFullScreen();
+    }
+    else if( element.msRequestFullscreen ) {
+      element.msRequestFullscreen();
+    }
+  };
+
+  if( fullscreenEnabled ) {
+    window.addEventListener( 'keyup', function( event ) {
+      if( event.key === 'F' ) {
+        requestFullscreen( document.documentElement );
+      }
+    });
+  }
+}());
